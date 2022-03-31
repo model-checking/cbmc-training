94162766ffffb28cc364494463dbfd1d2434447b
markrtuttle <tuttle@acm.org>
2022-03-31 15:03:11 +0000

    Deploying to gh-pages from @ model-checking/cbmc-training@f02ef17937f83177553f33c570b9b5a36128c062 🚀

---
+++ cbmc/overview/examples/simple/termination.c
@@ -0,0 +1,13 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+#include <stdlib.h>
+main() {
+  size_t initial_size;
+  size_t requested_size;
+
+  size_t size = initial_size;
+  while (size < requested_size) {
+    size *= 2;
+  }
+}
